ef268e10bf71a0535cfb4629dd712528a80ac82a
Gairaud <51027552+Gairaud@users.noreply.github.com>
2021-09-11 23:23:51 -0600

    Main Entities

---
+++ src/main/java/com/glob/ufriends/entities/UserPhoto.java
@@ -0,0 +1,23 @@
+package com.glob.ufriends.entities;
+
+/*import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USER_PHOTOS")
+public class UserPhoto {
+    @Id
+    @OneToOne
+    @JoinColumn(name = "USER_UID", nullable = false)
+    private User userUid;
+    @Lob
+    @Column(name = "PROFILE_PIC")
+    private byte[] profilePic;
+
+}*/
