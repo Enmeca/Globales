8272cabf7a24b0390261e4387c5f04ffffad5011
Java64x <55204353+Java64x@users.noreply.github.com>
2021-09-12 23:39:03 -0600

    Started working on backend for User

---
+++ src/main/java/com/glob/ufriends/apis/UserController.java
@@ -0,0 +1,52 @@
+package com.glob.ufriends.apis;
+
+import com.glob.ufriends.entities.User;
+import com.glob.ufriends.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping(value = "/api/v1/user")
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return service.saveUser(user);
+    }
+
+    @PostMapping(path = "/loginV1")
+    public User loginById(@RequestBody User user){
+        return service.loginById(user.getId(),user.getPassword());
+    }
+
+    @PostMapping(path = "/loginV2")
+    public User loginByEmail(@RequestBody User user){
+        return service.loginByEmail(user.getEmail(),user.getPassword());
+    }
+
+    @GetMapping
+    public List<User> findAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping(path = "{id}")
+    public User findUserById(@PathVariable("id") String id) {
+        return service.getUser(id);
+    }
+
+    @GetMapping(path = "/All")
+    public List<User> findAllTutors() {
+        return service.getAllTutors();
+    }
+
+    @DeleteMapping
+    public void deleteUser(@RequestBody User user){
+        service.deleteUser(user);
+    }
+    
+}
