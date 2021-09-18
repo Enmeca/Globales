64d514d18dc044f1019e5eea4594b0cc638446ae
Java64x <55204353+Java64x@users.noreply.github.com>
2021-09-17 20:00:38 -0600

    Update UniversityController.java
    @RestController was not added by the person that did this controller so it was not working

--- src/main/java/com/glob/ufriends/apis/UniversityController.java
+++ src/main/java/com/glob/ufriends/apis/UniversityController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping(value = "/api/v1/university")
+@RestController
 public class UniversityController {
 
     @Autowired
