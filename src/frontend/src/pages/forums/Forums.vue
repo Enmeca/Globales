34f82aafed9a7667782321d02b0be4b33c208bfb
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-09-28 03:53:05 -0600

    init forums

--- src/frontend/src/pages/forums/Forums.vue
+++ src/frontend/src/pages/forums/Forums.vue
@@ -1,33 +1,92 @@
 <template>
   <center>
-    <b-card class="main-card text-light">
-      <b-card-title class="h2">
-        Foros
-        <b-icon-signpost-fill />
-      </b-card-title>
-      <b-card-body>
-        <b-card-text>
-          <p>
-            Desde aqui puedes iniciar un nuevo foro, asi como ver otros que
-            hayan creado
-          </p>
-        </b-card-text>
-      </b-card-body>
-    </b-card>
+    <div class="container-forum">
+      <b-card>
+        <b-input-group class="mt-1">
+          <b-form-input
+            v-model="newForum.titulo"
+            placeholder="¿Titulo del foro o pregunta?"
+          >
+          </b-form-input>
+        </b-input-group>
+        <b-input-group class="mt-1">
+          <b-form-textarea
+            id="textarea"
+            v-model="newForum.descripcion"
+            placeholder="Ingresa información adicional"
+            rows="3"
+            max-rows="3"
+          ></b-form-textarea>
+        </b-input-group>
+        <b-row class="mt-2 justify-content-around">
+          <b-form-checkbox
+            v-b-tooltip.hover
+            title="Anónimo"
+            switch
+            size="lg"
+            class="mt-1"
+          >
+            <b-icon-sunglasses scale="2" class="ml-2" />
+          </b-form-checkbox>
+          <b-button variant="info"> Publicar</b-button>
+        </b-row>
+      </b-card>
+      <ItemForum
+        v-for="forum in forums"
+        :key="forum.id"
+        :data="forum"
+        class="mt-2"
+      />
+    </div>
   </center>
 </template>
 
 <script>
-export default {};
+// Import components
+import ItemForum from "./ItemForum.vue";
+
+export default {
+  data() {
+    return {
+      newForum: {
+        titulo: "",
+        descripcion: "",
+        fecha: "28/09/2021",
+        anonimo: false,
+      },
+      forums: [
+        {
+          id: 1,
+          autor: "Juan Salas",
+          titulo: "¿Pasos iniciales para aprender c++ rapido?",
+          descripcion: "Estoy llevando el curso de fundamentos...",
+          fecha: "28/09/2021",
+          anonimo: false,
+        },
+        {
+          id: 2,
+          autor: "Daniela Delgado",
+          titulo: "¿Como matricular en verano y no morir?",
+          descripcion: "No pase el curso de programacion 1...",
+          fecha: "25/09/2021",
+          anonimo: true,
+        },
+      ],
+    };
+  },
+  // Specify which components you are using
+  components: {
+    ItemForum,
+  },
+};
 </script>
 
 <style scoped>
 center {
-  margin: 5vw;
-  margin-inline: 15vw;
-}
-.main-card {
   background-color: rgba(0, 0, 0, 0.5);
+}
+.container-forum {
+  margin-inline: 20vw;
 }
 p {
   font-size: 20px;
---
+++ src/frontend/src/pages/forums/ItemForum.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-card class="forum">
+    <strong>
+      {{ data.titulo }}
+    </strong>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.forum:hover {
+  background-color: rgb(235, 235, 235);
+}
+</style>
