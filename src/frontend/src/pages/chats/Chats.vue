ab442688387dc87938191038f040606216d62666
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-10-17 01:05:16 -0600

    Update Chats.vue

--- src/frontend/src/pages/chats/Chats.vue
+++ src/frontend/src/pages/chats/Chats.vue
@@ -1,30 +1,111 @@
 <template>
-  <center>
-    <b-card class="main-card text-light">
-      <b-card-title class="h2">
-        Chats
-        <b-icon-chat-dots-fill />
-      </b-card-title>
-      <b-card-body>
-        <b-card-text>
-          <p>
-            Desde aqui podras chatear con personas con las que hayas hecho match
-          </p>
-        </b-card-text>
-      </b-card-body>
-    </b-card>
-  </center>
+  <div id="chats-page">
+    <center>
+      <b-card class="main-card">
+        <b-row class="chats">
+          <b-col sm="12" md="4" lg="4" class="p-1" v-show="showChats">
+            <b-input-group>
+              <b-input-group-prepend is-text>
+                <b-icon-search />
+              </b-input-group-prepend>
+              <b-form-input v-model="searchField"> </b-form-input>
+            </b-input-group>
+            <b-card v-for="chat in chats" :key="chat.id" class="p-0 mt-1">
+              <b-row>
+                <b-col cols="4">
+                  <b-avatar
+                    v-if="true"
+                    variant="info"
+                    text="PS"
+                    size="lg"
+                  ></b-avatar>
+                  <b-avatar
+                    v-else
+                    variant="dark"
+                    icon="sunglasses"
+                    size="lg"
+                  ></b-avatar>
+                </b-col>
+                <b-col cols="8">
+                  <b-row>
+                    <b-col> persona </b-col>
+                    <b-col> Fecha </b-col>
+                  </b-row>
+                  <b-row>
+                    <b-col> Mensaje </b-col>
+                    <b-col> cant </b-col>
+                  </b-row>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+          <b-col sm="12" md="8" lg="8" class="p-1" v-show="showCurrentChat">
+            <b-card style="height: 100%">
+              <b-card-body v-if="actualChat != null">
+                Chat actual
+                {{ window.width }}
+                X
+                {{ window.height }}
+              </b-card-body>
+              <b-card-body v-else> Welcome to page chat </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-card>
+    </center>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      window: {
+        width: 0,
+        height: 0,
+      },
+      searchField: "",
+      chats: [
+        { user: "aja", id: 1 },
+        { user: "aja", id: 2 },
+        { user: "aja", id: 3 },
+        { user: "aja", id: 4 },
+      ],
+      actualChat: null, //{ user: "aja", id: 1 },
+    };
+  },
+  created() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.window.width = window.innerWidth;
+      this.window.height = window.innerHeight;
+    },
+  },
+  computed: {
+    showCurrentChat() {
+      return this.window.width >= 770 || this.actualChat != null;
+    },
+    showChats() {
+      return this.window.width >= 770 || this.actualChat == null;
+    },
+  },
+};
 </script>
 
 <style scoped>
-.main-card {
-  background-color: rgba(0, 0, 0, 0.5);
+#chats-page,
+center {
+  height: 70%;
+  margin-inline: 1vw;
+  margin-block: 2vw;
 }
-p {
-  font-size: 20px;
+.main-card {
+  background-color: rgba(0, 0, 0, 0.6);
 }
 </style>
